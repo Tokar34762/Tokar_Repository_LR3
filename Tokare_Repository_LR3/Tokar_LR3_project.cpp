f435aabc10a69cd752485bc3ae45519eeafb9f22
Tokar34762 <1bkbk1.ru@bk.ru>
2025-04-27 19:14:43 +0300

    create main

---
+++ Tokare_Repository_LR3/Tokar_LR3_project.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void inputArraySize(int &size) {}
+
+void inputArrayValues(vector<int> array, int size) {}
+
+int sumArray(vector<int> array) { return 0; }
+
+double averageArray(vector<int> array) { return 0.0; }
+
+int main() {
+  int choice;
+  int size;
+  vector<int> array;
+
+  do {
+    cout << "Menu:\n";
+    cout << "1. Input array size\n";
+    cout << "2. Input array values\n";
+    cout << "3. Find sum of all elements of array\n";
+    cout << "4. Find average value of array\n";
+    cout << "0. Exit\n";
+    cout << "Choose the menu item: ";
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+      inputArraySize(size);
+      array.resize(size);
+      break;
+    case 2:
+      inputArrayValues(array, size);
+      break;
+    case 3: {
+      int sum = sumArray(array);
+      cout << "The sum of all array elements: " << sum << endl;
+      break;
+    }
+    case 4: {
+      double average = averageArray(array);
+      cout << "Average value of array elements: " << average << endl;
+      break;
+    }
+    case 0:
+      cout << "Exiting the program." << endl;
+      break;
+    default:
+      cout << "Invalid choice. Please try again." << endl;
+    }
+  } while (choice != 0);
+
+  return 0;
+}
